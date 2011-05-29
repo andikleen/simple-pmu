1b3a550ce26848ec1ee6bdbfe498ee8d7902ec2a
Andi Kleen <ak@linux.intel.com>
2011-05-28 21:49:41 -0700

    improve user space

--- cycles.h
+++ cycles.h
@@ -1,6 +1,15 @@
 #ifndef CYCLES_H
 #define CYCLES_H 1
 
+#if defined(_SCHED_H) && !defined(__USE_GNU)
+#error "Add #define _GNU_SOURCE 1 at beginning of source file"
+#endif
+
+#define _GNU_SOURCE 1
+#include <sched.h>
+
+#define force_inline __attribute__((always_inline))
+
 typedef unsigned long long counter_t;
 
 static inline void p_cpuid(unsigned in, 
@@ -17,14 +26,14 @@ static inline unsigned p_cpuid_a(unsigned in)
 	return a;
 }
 
-static inline void sync_core(void)
+static inline force_inline void sync_core(void)
 {
 	unsigned a, b, c, d;
 	asm volatile("cpuid" : "=a" (a), "=b" (b), "=c" (c), "=d" (d)
 		    	     : "0" (0) : "memory");
 }
 
-static inline unsigned long long p_rdpmc(unsigned in)
+static inline force_inline unsigned long long p_rdpmc(unsigned in)
 {
 	unsigned d, a;
 
@@ -50,19 +59,63 @@ enum {
 	FIXED_CPU_CLK_UNHALTED_REF = 2,
 };
 
-static inline counter_t unhalted_core(void)
+static inline force_inline counter_t unhalted_core(void)
 {
 	return p_rdpmc(FIXED_SELECT|FIXED_CPU_CLK_UNHALTED_CORE);
 }
 
-static inline counter_t unhalted_ref(void)
+static inline force_inline counter_t unhalted_ref(void)
 {
 	return p_rdpmc(FIXED_SELECT|FIXED_CPU_CLK_UNHALTED_REF);
 }
 
-static inline counter_t insn_retired(void)
+static inline force_inline counter_t insn_retired(void)
 {
 	return p_rdpmc(FIXED_SELECT|FIXED_INST_RETIRED_ANY);
+}
+
+/* Lots of caveats when you use that */
+
+static inline force_inline unsigned long long rdtsc(void)
+{
+#ifdef __i386__
+	unsigned long long s;
+	asm volatile("rdtsc" : "=A" (s) :: "memory");
+	return s;
+#else
+	unsigned low, high;
+	asm volatile("rdtsc" : "=a" (low), "=d" (high) :: "memory");
+	return ((unsigned long long)high << 32) | low;
+#endif
+}
+
+static inline force_inline unsigned long long rdtscp(void)
+{
+#ifdef __i386__
+	unsigned long long s;
+	asm volatile("rdtscp" : "=A" (s) :: "ecx", "memory");
+	return s;
+#else
+	unsigned low, high;
+	asm volatile("rdtscp" : "=a" (low), "=d" (high) :: "ecx", "memory");
+	return ((unsigned long long)high << 32) | low;
+#endif
+}
+
+static inline int pin_cpu(cpu_set_t *oldcpus)
+{
+	int cpu = sched_getcpu();
+	cpu_set_t cpus;
+	CPU_ZERO(&cpus);
+	CPU_SET(cpu, &cpus);
+	if (oldcpus)
+		sched_getaffinity(0, sizeof(cpu_set_t), oldcpus);
+	return sched_setaffinity(0, sizeof(cpu_set_t), &cpus);
+}
+
+static inline void unpin_cpu(cpu_set_t *oldcpus)
+{
+	sched_setaffinity(0, sizeof(cpu_set_t), oldcpus);
 }
 
 #endif
--- tcyc.c
+++ tcyc.c
@@ -1,5 +1,7 @@
+#define _GNU_SOURCE 1
 #include <stdio.h>
 #include <stdlib.h>
+#include <sched.h>
 #include "cycles.h"
 
 volatile float f;
@@ -12,8 +14,12 @@ static inline void kernel(void)
 
 int main(void)
 {
-	unsigned long long a, b;
+	counter_t a, b;
 
+	if (pin_cpu(NULL) < 0) { 
+		printf("Cannot pin CPU\n");
+		exit(1);
+	}
 	if (perfmon_available() <= 0) {
 		printf("no fixed perfmon available\n");
 		exit(1);
@@ -25,12 +31,14 @@ int main(void)
 	sync_core();
 	printf("unhalted cycles %llu\n", b-a);	
 
+#if 0	/* gone on nehalem */
 	sync_core();
 	a = unhalted_ref();
 	kernel();
 	b = unhalted_ref();
 	sync_core();
 	printf("reference cycles %llu\n", b-a);	
+#endif
 
 	sync_core();
 	a = insn_retired();
@@ -38,6 +46,12 @@ int main(void)
 	b = insn_retired();
 	sync_core();
 	printf("instructions retired %llu\n", b-a);	
+
+	sync_core();
+	a = rdtsc();
+	kernel();
+	b = rdtsc();
+	printf("rdtsc tick %llu\n", b-a);
 
 	return 0;	
 
